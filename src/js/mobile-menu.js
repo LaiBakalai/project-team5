998c5d874b2e6b3153e80a72a974f1b7f3453d08
LaiBakalai <107401798+LaiBakalai@users.noreply.github.com>
2023-10-24 18:44:26 +0800

    added

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -1,3 +1,4 @@
+
 (() => {
     const mobileMenu = document.querySelector('.js-menu-container');
     const openMenuBtn = document.querySelector('.js-open-menu');
@@ -55,3 +56,24 @@
     bodyScrollLock.enableBodyScroll(document.body);
   });
 })();
+
+/*
+(() => {
+  const menuBtnRef = document.querySelector('[data-menu-button]');
+  const mobileMenuRef = document.querySelector('[data-menu]');
+  const buttonTablet = document.querySelector('[data-button-tablet]');
+
+  menuBtnRef.addEventListener('click', () => {
+    const expanded =
+      menuBtnRef.getAttribute('aria-expanded') === 'true' || false;
+
+    menuBtnRef.classList.toggle('is-open');
+    buttonTablet.classList.toggle('is-hidden');
+    menuBtnRef.setAttribute('aria-expanded', !expanded);
+
+    mobileMenuRef.classList.toggle('is-open');
+    menuBtnRef.classList.toggle('is-fixed');
+  });
+})();
+
+*/
