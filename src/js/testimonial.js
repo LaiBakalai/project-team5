a6721e293bb996379113f756eb078201ec237ab4
khokenneth31 <khokenneth26@gmail.com>
2023-10-24 21:33:39 +0800

    testimonials

--- src/js/testimonial.js
+++ src/js/testimonial.js
@@ -10,7 +10,7 @@ function currentSlide(n) {
 function showSlides(n) {
   var i;
   var slides = document.getElementsByClassName('mySlides');
-  var dots = document.getElementsByClassName('testi-icon-dots1');
+  var dots = document.getElementsByClassName('dot');
   if (n > slides.length) {
     slideIndex = 1;
   }
